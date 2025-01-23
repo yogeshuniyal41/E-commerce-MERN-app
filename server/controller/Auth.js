2e964d8ea3f7b50c1ac201ef9d375199331df64e
yogeshuniyal41 <yuniyal41@gmail.com>
2025-01-23 21:13:53 +0530

    .

--- server/controller/Auth.js
+++ server/controller/Auth.js
@@ -79,7 +79,7 @@ exports.resetPasswordRequest = async (req, res) => {
 
     // Also set token in email
     const resetPageLink =
-      'https://mernappecomm.netlify.app/reset-password?token=' + token + '&email=' + email;
+      '/reset-password?token=' + token + '&email=' + email;
     const subject = 'reset password for e-commerce';
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset Password</p>`;
 
